11d8b7feb56a78dede5a2f5014c59f27c17fb0e9
S.Percentage <2141528+Pctg-x8@users.noreply.github.com>
2025-09-20 12:45:14 +0900

    add: notice for build

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
+    // Note:
+    // Gentooとかは別のslangパッケージとライブラリ名が被ってるのでSLANG_LIB_FULLPATHを指定することでサーチパスを上書きする必要がある
     println!("cargo::rerun-if-env-changed=SLANG_LIB_FULLPATH");
     if let Some(libpath) = std::env::var_os("SLANG_LIB_FULLPATH") {
         println!("cargo::rustc-link-search={}", libpath.display());
